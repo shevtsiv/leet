ceb71cfdac67ce8007221364d33920aaf5cf8dc5
shevtsiv <rostykshevtsiv@gmail.com>
2020-08-28 20:19:59 +0300

    26. Remove Duplicates from Sorted Array [Using 2 Pointers]

--- src/main/kotlin/easy/remove-duplicates-from-sorted-array.kt
+++ src/main/kotlin/easy/remove-duplicates-from-sorted-array.kt
@@ -1,11 +1,28 @@
 package easy
 
 class RemoveDuplicatesFromSortedArraySolution {
+
+    /**
+     * Time Complexity: O(n)
+     * Space Complexity: O(1)
+     */
+    fun removeDuplicatesUsingTwoPointers(nums: IntArray): Int {
+        var leftPointer = 0
+        var rightPointer = 0
+        while (rightPointer < nums.size) {
+            if (nums[leftPointer] != nums[rightPointer]) {
+                nums[++leftPointer] = nums[rightPointer]
+            }
+            rightPointer++
+        }
+        return leftPointer + 1
+    }
+
     /**
      * Time Complexity: O(n^3)
      * Space Complexity: O(1)
      */
-    fun removeDuplicates(nums: IntArray): Int {
+    fun removeDuplicatesByRemovingAndShiftingElements(nums: IntArray): Int {
         var size = nums.size
         for (i in 0 until size - 1) {
             while (nums[i] == nums[i + 1] && i < size - 1) {

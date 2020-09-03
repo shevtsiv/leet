f4188d5d9359eeb2a2ed9a974e7c0dd2b1d83762
shevtsiv <rostykshevtsiv@gmail.com>
2020-09-03 21:52:47 +0300

    Implement strStr() [Using 2 Pointers]
    Equivalent to bruteforce solution, unfortunately.

--- src/main/kotlin/easy/implement-str-str.kt
+++ src/main/kotlin/easy/implement-str-str.kt
@@ -6,6 +6,33 @@ class ImplementStrStrSolution {
      * Time Complexity: O(n^2)
      * Space Complexity: O(1)
      */
+    fun strStrUsingTwoPointers(haystack: String, needle: String): Int {
+        if (needle.isEmpty()) {
+            return 0
+        }
+        var j = 0
+        var i = 0
+        while (i < haystack.length) {
+            if (haystack[i] == needle[j]) {
+                j++
+                if (j == needle.length) {
+                    return i + 1 - j
+                }
+            } else {
+                if (j != 0) {
+                    i -= j
+                    j = 0
+                }
+            }
+            i++
+        }
+        return -1
+    }
+
+    /**
+     * Time Complexity: O(n^2)
+     * Space Complexity: O(1)
+     */
     fun strStr(haystack: String, needle: String): Int {
         if (haystack.isEmpty() && needle.isEmpty()) {
             return 0

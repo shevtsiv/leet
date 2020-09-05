3000289983db7c0135942155904ac7892b05d0c8
shevtsiv <rostykshevtsiv@gmail.com>
2020-09-05 23:26:30 +0300

    Search Insert Position [Using Binary Search]

--- src/main/kotlin/easy/search-insert-position.kt
+++ src/main/kotlin/easy/search-insert-position.kt
@@ -3,6 +3,30 @@ package easy
 class SearchInsertPositionSolution {
 
     /**
+     * Time Complexity: O(log(n))
+     * Space Complexity: O(1)
+     */
+    fun searchInsertUsingBinarySearch(nums: IntArray, target: Int): Int {
+        var low = 0
+        var high = nums.size
+        while (low <= high) {
+            val mid = (low + high) / 2
+            when {
+                nums[mid] < target -> {
+                    low = mid + 1
+                }
+                nums[mid] > target -> {
+                    high = mid - 1
+                }
+                else -> {
+                    return mid
+                }
+            }
+        }
+        return low
+    }
+
+    /**
      * Time Complexity: O(n)
      * Space Complexity: O(1)
      */

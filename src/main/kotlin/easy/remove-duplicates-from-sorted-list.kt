a0daab57185bb4dee9796ee8339ecc33899b4bee
shevtsiv <rostykshevtsiv@gmail.com>
2020-09-15 22:42:08 +0300

    Remove Duplicates from Sorted List [Two Pointers]

--- src/main/kotlin/easy/remove-duplicates-from-sorted-list.kt
+++ src/main/kotlin/easy/remove-duplicates-from-sorted-list.kt
@@ -6,6 +6,25 @@ class RemoveDuplicatesFromSortedListSolution {
      * Time Complexity: O(n)
      * Space Complexity: O(1)
      */
+    fun deleteDuplicatesUsingTwoPointers(headInput: ListNode?): ListNode? {
+        var slow = headInput
+        var fast = headInput
+        while (fast != null) {
+            if (slow!!.`val` != fast.`val`) {
+                slow.next = fast
+                slow = slow.next
+            } else {
+                fast = fast.next
+            }
+        }
+        slow!!.next = null // cut off the rest of the list after the fast pointer reached the end
+        return headInput
+    }
+
+    /**
+     * Time Complexity: O(n)
+     * Space Complexity: O(1)
+     */
     fun deleteDuplicates(headInput: ListNode?): ListNode? {
         var head = headInput
         while (head != null) {

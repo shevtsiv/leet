76a7dd83db5164d05bc6a36924876032cc7fe3f8
shevtsiv <rostykshevtsiv@gmail.com>
2020-10-12 20:02:35 +0300

    Valid Anagram [Two Pass and Using Array]

--- src/main/kotlin/easy/valid-anagram.kt
+++ src/main/kotlin/easy/valid-anagram.kt
@@ -33,4 +33,54 @@ class ValidAnagramSolution {
         }
         return true
     }
+
+    /**
+     * Time Complexity: O(2n) => O(n), where n - length of one of the input strings
+     * Space Complexity: O(n) => O(n), where n - length of one of the input strings
+     */
+    fun isAnagramTwoPass(s: String, t: String): Boolean {
+        if (s.length != t.length) {
+            return false
+        }
+        val table = HashMap<Char, Int>(s.length)
+        for (char in s) {
+            if (table.contains(char)) {
+                table[char] = table[char]!! + 1
+            } else {
+                table[char] = 1
+            }
+        }
+        for (char in t) {
+            if (table.contains(char)) {
+                table[char] = table[char]!! - 1
+                if (table[char]!! < 0) {
+                    return false
+                }
+            } else {
+                return false
+            }
+        }
+        return true
+    }
+
+    /**
+     * Time Complexity: O(2n) => O(n), where n - length of one of the input strings
+     * Space Complexity: O(1)
+     */
+    fun isAnagramUsingArray(s: String, t: String): Boolean {
+        if (s.length != t.length) {
+            return false
+        }
+        val table = IntArray(26)
+        for (char in s) {
+            table[char - 'a'] += 1
+        }
+        for (char in t) {
+            table[char - 'a'] -= 1
+            if (table[char - 'a'] < 0) {
+                return false
+            }
+        }
+        return true
+    }
 }
